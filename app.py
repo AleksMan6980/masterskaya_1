761fb38addcf6f0587713a5dff419a1316b5c7ad
AleksMan6980 <alekseev.ilya.v@yandex.ru>
2025-06-07 12:53:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,18 @@ import io
 
 app = FastAPI()
 
+# получаем из ноутбука с моделью ()
+# import joblib
+# joblib.dump(model.best_estimator_, './final_model.pkl')
 model = joblib.load('final_model.pkl')
 
+# конечная точка маршрута запроса curl (программы, формирующей запрос)
 @app.post("/predict")
-async def predict(file: UploadFile = File(...)):
-    content = await file.read()
-    df = pd.read_csv(io.StringIO(content.decode('utf-8')))
+
+# функция, в которую передается запрос
+async def predict(file: UploadFile = File(...)): # запрос содержит файл с данными
+    content = await file.read() # чтение байт строки
+    df = pd.read_csv(io.StringIO(content.decode('utf-8'))) # создание датасета с декодирование строки в csv файл из потока декодированных данных
     df = df.drop(['Unnamed: 0', 'Family History'], axis = 1)
     df.columns = map(str.lower, df.columns)
     df.columns = df.columns.str.replace('(', '')
